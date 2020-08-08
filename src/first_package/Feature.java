6b362616af33219359a49af184d2156982a4d250
Husna Ceren <hcyesilcicek61@gmail.com>
2020-08-08 17:06:03 -0300

    the third commit from local master

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -6,7 +6,7 @@ public class Feature {
 		System.out.println("This is my commit from feature branch");
 		System.out.println("This isthe first commit from remote master branch");
 		System.out.println("This is the second commit from the remote master branch");
-		
+		System.out.println("This the third commit from local master");
 
 	}
 
